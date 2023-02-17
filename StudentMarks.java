ff21db14ce6a9beb0879426b18b4e482ac4217e0
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-02-17 18:25:06 +0530

    Completed Q13

--- StudentMarks.java
+++ StudentMarks.java
@@ -3,8 +3,23 @@ package exercise_1;
 public class StudentMarks {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[][] marks = new int[][] { 
+			{80, 85, 88, 90, 91}, 
+			{81, 82, 85, 87, 91}, 
+			{75, 66, 87, 85, 91}
+			};
+			
+		int index = 1;
+		for(int[] arr: marks) {
+			int score = 0;
+			
+			for(int ele: arr) {
+				score += ele;
+			}
+			System.out.print("Score of student " + String.valueOf(index) + " is: ");
+			System.out.println(score);
+			++index;
+		}
 	}
 
 }
