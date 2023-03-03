6159ae21cd40993956bf70d026b41c91a2690c1c
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-03-03 17:00:46 +0530

    Modifications following comments from reviews

--- exercise_4/creational_pattern/FactoryPattern.java
+++ exercise_4/creational_pattern/FactoryPattern.java
@@ -3,47 +3,67 @@ package exercise_4.creational_pattern;
 public class FactoryPattern {
 
 	public static void main(String[] args) {
-		Printer press = new ScienceMagazinePress();
-		Magazine mg1 = press.getMagazine();
 		
-		Person p1 = new Person();
+		Person p1 = new Person("Student");
+		Magazine mg1 = MagazineShop.buyMagazine(p1);
 		p1.read(mg1);
 		
-		press = new BusinessMagazinePress();
-		mg1 = press.getMagazine();
+		p1 = new Person("Business Man");
+		mg1 = MagazineShop.buyMagazine(p1);
 		p1.read(mg1);
 		
 	}
 }
 
 class Person{
+	String type;
+	
+	Person(String type){
+		this.type = type;
+	}
+	
 	public void read(Magazine mg) {
+		if(mg == null) System.out.println("No magazine to read");
 		System.out.println(mg.getContent());
 	}
-}
-
-
-interface Printer{
-	public Magazine getMagazine();
-}
-
-class ScienceMagazinePress implements Printer{
 	
-	ScienceMagazinePress() {}
-	
-	public Magazine getMagazine() {
-		return new ScienceMagazine();
+	public String getType() {
+		return type;
 	}
-	
 }
 
-class BusinessMagazinePress implements Printer{
-
-	public Magazine getMagazine() {
-		return new BusinessMagazine();
+class MagazineShop{
+	public static Magazine buyMagazine(Person person) {
+		if(person.getType() == "Student") {
+			return new ScienceMagazine();
+		}
+		if(person.getType() == "Business Man") {
+			return new BusinessMagazine();
+		}
+		return null;
 	}
-	
 }
+
+
+//interface Printer{
+//	public Magazine getMagazine();
+//}
+//
+//class ScienceMagazinePress implements Printer{
+//	
+//	public Magazine getMagazine() {
+//		return new ScienceMagazine();
+//	}
+//	
+//}
+//
+//class BusinessMagazinePress implements Printer{
+//
+//	public Magazine getMagazine() {
+//		return new BusinessMagazine();
+//	}
+//	
+//}
 
 
 interface Magazine{
